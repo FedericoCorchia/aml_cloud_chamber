01b8c76533c944d8069c254c25ab9c42b0631098
Federico Andrea Corchia <fcorchia@cern.ch>
2024-07-12 10:20:23 +0200

    Change expected input image resolution in training.py

--- training.py
+++ training.py
@@ -6,23 +6,23 @@ from keras.layers import Dense, Input, Conv2D, MaxPooling2D, Flatten, Dropout, R
 from keras.preprocessing import image
 from keras.metrics import Recall
 
-#definition of folders with training and test samples
+#definition of folders with training and valid samples
 dataDir = str(sys.argv[1])
 trainDir = os.path.join(dataDir, "train")
-testDir = os.path.join(dataDir, "test")
+validDir = os.path.join(dataDir, "valid")
 print("dataDir:", dataDir)
 
 #pixel rescaling from [0, 255] to [0, 1]
 trainImageDataGenerator = image.ImageDataGenerator(rescale=1./255)
-testImageDataGenerator = image.ImageDataGenerator(rescale=1./255)
+validImageDataGenerator = image.ImageDataGenerator(rescale=1./255)
 
 #generators
-trainGenerator = trainImageDataGenerator.flow_from_directory(trainDir, target_size=(1024, 576), color_mode="grayscale", batch_size=20, class_mode="binary")
+trainGenerator = trainImageDataGenerator.flow_from_directory(trainDir, target_size=(768, 432), color_mode="grayscale", batch_size=20, class_mode="binary")
 trainClassIndices = trainGenerator.class_indices #checks that signal class has index 1 as metrics Recall refers by default to the class with index 1
 print("trainClassIndices:", trainClassIndices)
-testGenerator = testImageDataGenerator.flow_from_directory(testDir, target_size=(1024, 576), color_mode="grayscale", batch_size=20, class_mode="binary")
-testClassIndices = testGenerator.class_indices
-print("testClassIndices:", testClassIndices)
+validGenerator = validImageDataGenerator.flow_from_directory(validDir, target_size=(768, 432), color_mode="grayscale", batch_size=20, class_mode="binary")
+validClassIndices = validGenerator.class_indices
+print("validClassIndices:", validClassIndices)
 
 #data augmentation, as the dataset is still too small for proper training
 dataAugmentation = Sequential([
@@ -33,7 +33,7 @@ dataAugmentation = Sequential([
 #model definition
 model = Sequential()
 model.add(dataAugmentation)
-model.add(Conv2D(32, (3, 3), activation="relu", padding="same", name="conv_1", input_shape=(1024, 576, 1)))
+model.add(Conv2D(32, (3, 3), activation="relu", padding="same", name="conv_1", input_shape=(768, 432, 1)))
 model.add(MaxPooling2D((2, 2), name="maxpool_1"))
 model.add(Conv2D(64, (3, 3), activation="relu", padding="same", name="conv_2"))
 model.add(MaxPooling2D((2, 2), name="maxpool_2"))
@@ -51,7 +51,7 @@ model.add(Dense(1, activation="sigmoid", name="output"))
 model.compile(optimizer="adam", loss="binary_crossentropy", metrics=[Recall()])
 
 #training
-history = model.fit(trainGenerator, steps_per_epoch=100, epochs=50, validation_data=testGenerator, validation_steps=50, verbose=1)
+history = model.fit(trainGenerator, steps_per_epoch=100, epochs=50, validation_data=validGenerator, validation_steps=50, verbose=1)
 
 #model saving
 model.save("savedModel/model")
